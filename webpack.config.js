ce9c5d46c235d3eb06c916a30c843122ea710d6f
RuqayaAbdulhaq <rouqayaabdulhaq@gmail.com>
2024-03-14 21:17:54 +0300

    removed node modules

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './chatWidget.ts',
+  output: {
+    filename: 'chatWidget.min.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
